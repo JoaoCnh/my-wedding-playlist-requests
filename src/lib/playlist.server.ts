691cfc950c70a522028d265a0a41a2b78673e541
João Miguel Marinho da Cunha <joaocunha@Joaos-MacBook-Pro.local>
2023-02-03 10:37:32 +0000

    📦 Add types export for playlist requests

--- src/lib/playlist.server.ts
+++ src/lib/playlist.server.ts
@@ -8,15 +8,7 @@ import {
 
 import { supabase } from "~/lib/supabase.server";
 
-interface PlaylistRequest {
-  spotify_id: string;
-  track_name: string;
-  track_artists: string;
-  track_small_img: string;
-  track_large_img: string;
-  track_preview_url: string;
-  track_requesters: string;
-}
+import type { PlaylistRequest } from "~/types";
 
 const lruCache = new LRUCache<string, CacheEntry<PlaylistRequest[]>>({
   max: 1,
@@ -31,7 +23,6 @@ export async function getPlaylistRequests(): Promise<PlaylistRequest[]> {
     ttl: 1000 * 60 * 60 * 24, // 24 hours,
     reporter: verboseReporter(),
     async getFreshValue() {
-      console.log("GETTING FRESH VALUE");
       const { data, error } = await supabase
         .from("playlist_requests")
         .select<"*", PlaylistRequest>();
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface PlaylistRequest {
+  spotify_id: string;
+  track_name: string;
+  track_artists: string;
+  track_small_img: string;
+  track_large_img: string;
+  track_preview_url: string;
+  track_requesters: string;
+}
