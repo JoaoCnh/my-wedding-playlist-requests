1cc3c7150a91dee649dce65d6eb3fdac6a517839
João Miguel Marinho da Cunha <joaocunha@Joaos-MacBook-Pro.local>
2023-02-03 17:35:39 +0000

    ▲ Set cache-control header

--- src/root.tsx
+++ src/root.tsx
@@ -11,6 +11,7 @@ import {
   Scripts,
   Title,
 } from "solid-start";
+import { HttpHeader } from "solid-start/server";
 
 import Footer from "./components/Footer";
 
@@ -19,23 +20,29 @@ import "./client-polyfills";
 
 export default function Root() {
   return (
-    <Html lang="en" class="h-screen">
-      <Head>
-        <Title>My Wedding Playlist Requests</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body class="h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-orange-400 to-rose-400">
-        <Suspense>
-          <ErrorBoundary>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-            <Footer />
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
-    </Html>
+    <>
+      <HttpHeader
+        name="Cache-Control"
+        value="s-maxage=86400000, stale-while-revalidate"
+      />
+      <Html lang="en" class="h-screen">
+        <Head>
+          <Title>My Wedding Playlist Requests</Title>
+          <Meta charset="utf-8" />
+          <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Body class="h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-orange-400 to-rose-400">
+          <Suspense>
+            <ErrorBoundary>
+              <Routes>
+                <FileRoutes />
+              </Routes>
+              <Footer />
+            </ErrorBoundary>
+          </Suspense>
+          <Scripts />
+        </Body>
+      </Html>
+    </>
   );
 }
