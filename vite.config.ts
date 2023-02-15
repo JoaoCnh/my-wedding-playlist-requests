88a59cc13fa1801c2f6f50c0e05e5a7ea08d11e6
João Miguel Marinho da Cunha <joaocunha@Joaos-MacBook-Pro.local>
2023-02-15 09:55:54 +0000

    📦 Update packages and remove vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import solid from "solid-start/vite";
-// @ts-ignore
-import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel() })],
+  plugins: [solid()],
 });
